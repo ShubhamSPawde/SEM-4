aa5ac515a5944a602c9a36a4be6a63d404291f3f
ShubhamSPawde <shubhamspawade@gmail.com>
2025-02-03 23:47:55 +0530

    Upload

---
+++ JAVA_OOPS/02_Assignment.java
@@ -0,0 +1,7 @@
+// Write a Java program that simulates a simple Library Management System using Object-Oriented Programming (OOP) concepts such as classes, objects, methods, and constructors. The program should allow users to manage books in the library by performing operations like,
+
+// Adding new books
+// Removing books, 
+// Displaying the list of books, 
+// Searching for books.
+
